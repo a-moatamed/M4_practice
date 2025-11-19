2798e36d8918db49af4564f0a0b2b4fb23af0ea4
abdo <mac@gmail.com>
2025-11-19 22:41:18 +0200

    freeing main.c from any startup codes and code them in a startup file

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS  ?=  -W -Wall -Wextra -Werror -Wundef -Wshadow -Wdouble-promotion \
             -g3 -Os -ffunction-sections -fdata-sections -I. -Iinclude \
             -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 $(EXTRA_CFLAGS)
 LDFLAGS ?= -Tlink.ld -nostartfiles -nostdlib --specs nano.specs -lc -lgcc -Wl,--gc-sections -Wl,-Map=$@.map
-SOURCES = main.c src/gpio.c src/systick.c src/rcc.c
+SOURCES = main.c src/gpio.c src/systick.c src/rcc.c startup.c
 
 build: firmware.elf
 
--- main.c
+++ main.c
@@ -29,17 +29,3 @@ int main(void) {
         
     }
 }
-
-__attribute__((naked, noreturn)) void _reset(void) {
-    extern long _sbss, _ebss, _sdata, _edata, _sidata;
-    for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
-    for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
-    main();
-    for (;;) (void)0;
-}
-
-extern void _reset(void);
-extern unsigned long _estack;
-__attribute__((section(".vectors"))) 
-void (*const tab[16 + 91])(void) = {
-    (void (*)(void))(&_estack), _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, SysTick_Handler};
