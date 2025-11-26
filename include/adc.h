3cfb83182293e4d199d75203e8f7da2cdb17c047
Your Name <you@example.com>
2025-11-26 14:46:52 +0200

    ading ADC driver

---
+++ include/adc.h
@@ -0,0 +1,18 @@
+#include "rcc.h"
+
+
+#define ADC ((struct *adc ) 0x50000000)
+
+struct adc{
+
+ISR, IER, CR, CFGR, CFGR2, SMPR1, SMPR2,
+	TR1, TR2, TR3, SQR1, SQR2, SQR3,SQR4,
+	DR, JSQR, OFRy, JDRy, AWD2CR, 
+	AWD3CR, DIFSEL, CALFACT;
+};
+
+void adc_init(struct *adc adc)
+{
+
+
+}
