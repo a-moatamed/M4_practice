7f42cc75bbeec92f15f73b3e5719cfc086dcf8cc
abdo <mac@gmail.com>
2025-12-06 10:43:15 +0200

    implementing USART driver succesfully and redirect the output to printf()

--- include/uart.h
+++ include/uart.h
@@ -18,18 +18,22 @@ struct uart
     volatile uint32_t CR1, CR2, CR3, BRR, GTPR, GTOR, RQR, ISR, ICR, RDR, TDR;
 };
 
+// function to check it the data is ready to read 
 static inline int uart_read_ready(struct uart *uart)
 {
     return uart->ISR & BIT(5); // If RXNE bit is set, data is ready
 }
 
-void uart_init(struct uart *uart, uint32_t baud);
 
+
+// function to read a byte if it's ready
 static inline uint8_t uart_read_byte(struct uart *uart)
 {
     return (uint8_t)(uart->RDR & 255);
 }
 
+
+// function to write a byte if it's ready
 static inline void uart_write_byte(struct uart *uart, uint8_t byte)
 {
     uart->TDR = byte;
@@ -37,8 +41,68 @@ static inline void uart_write_byte(struct uart *uart, uint8_t byte)
         spin(1);
 }
 
+
 static inline void uart_write_buf(struct uart *uart, char *buf, size_t len)
 {
     while (len-- > 0)
         uart_write_byte(uart, *(uint8_t *)buf++);
 }
+
+
+
+void uart_init(struct uart *uart, uint32_t baud)
+{
+    uint16_t tx = 0, rx = 0;
+    uint8_t af = (uart == UART4) ? 8 : 7;   // Default AF for most UARTs (including USART1) is 7
+
+    // Enable UART clock
+    if (uart == UART1) {
+        RCC->APB2ENR |= BIT(14);
+        tx = PIN('B', 6);
+        rx = PIN('B', 7);
+
+        
+    }
+    else if (uart == UART2) {
+        RCC->APB1ENR1 |= BIT(17);
+        // TODO: Set UART2 pins later
+    }
+    else if (uart == UART3) {
+        RCC->APB1ENR1 |= BIT(18);
+        // TODO: Set UART3 pins later.
+    }
+    else if (uart == UART4) {
+        RCC->APB1ENR1 |= BIT(19);
+        // TODO: UART4 pins (AF8)
+        
+    }
+    else if (uart == UART5) {
+        RCC->APB1ENR1 |= BIT(20);
+        // TODO: UART5 pins
+    }
+
+ 
+    gpio_set_mode(tx, GPIO_MODE_AF);
+    gpio_set_af(tx, af);
+
+    gpio_set_mode(rx, GPIO_MODE_AF);
+    gpio_set_af(rx, af);
+
+
+    // Configure UART
+    uart->CR1 = 0;                          
+    uart->BRR = FREQ / baud;                
+    uart->CR1 |= BIT(0) | BIT(2) | BIT(3); // set UE, RE, TE
+}
+
+
+
+
+// this function to redirct the write function in uart to printf which is more cabable and easy to expose more info
+
+int _write(int fd, char *ptr, int len) {
+  (void) fd, (void) ptr, (void) len;
+  if (fd == 1) uart_write_buf(UART1, ptr, (size_t) len);
+  return -1;
+}
+
--- main.c
+++ main.c
@@ -1,21 +1,56 @@
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "gpio.h"
 #include "helper.h"
 #include "rcc.h"
 #include "systick.h"
 #include "timer.h"
 #include "adc.h"
+#include "uart.h"
+
+#include <stdio.h>
+
+
+#define CLOCK 4000000
 
 volatile uint32_t s_ticks = 0;
 
 
-
+/**
+ * the main of this function is to to toggle a led.
+ * on-board led(LED2: the green led) is connected to the pin PB14.
+ */
 int main(void)
 {
+    // intialize the systick
+    systick_init(CLOCK/1000);
+    uint32_t timer = 0;
+    uint32_t period = 500;
 
+
+    // define the led pin
+    uint16_t led = PIN('B', 14);
+    // choose the mode ### note that the function will enable the RCC port 
+    gpio_set_mode(led, GPIO_MODE_OUTPUT);
+    static bool on = true; // set the flag of the led.
+
+    struct uart* uart = UART1;
+    uart_init(uart, 115200);
+    
+
+    
     while (1)
     {
+        if(timer_expired(&timer, period, s_ticks))
+        {
+
+            on = !on;
+            gpio_write(led, on);
+            printf("LED: %d, tick: %lu\r\n", on, s_ticks);  // Write message
+    
+        }
+
         
     }
 }
